a6112e4df825a377d12cfa4e325cacd786e82329
LHelge <249865+LHelge@users.noreply.github.com>
2023-11-20 15:54:14 +0100

    Day16 part 2 is running but not correct

--- 2022/day_16/src/lib.rs
+++ 2022/day_16/src/lib.rs
@@ -5,7 +5,8 @@ use valves::{parse_valves, Valve};
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone)]
 struct State<'a> {
-    position: &'a str,
+    my_position: &'a str,
+    elefant_position: &'a str,
     time_left: u32,
     opened: BTreeSet<&'a str>,
 }
@@ -14,6 +15,93 @@ enum Action<'a> {
     Open(&'a str),
     Move(&'a str),
     None,
+}
+
+// Recursive search algorithm with a HashMap for caching state
+fn max_pressure_released_with_elefant<'a>(
+    mut state: State<'a>,
+    valves: &HashMap<&str, Valve<'a>>,
+    cache: &mut HashMap<State<'a>, u64>,
+    my_action: &Action<'a>,
+    elefant_action: &Action<'a>,
+) -> u64 {
+    match my_action {
+        Action::Open(valve) => {
+            state.opened.insert(valve);
+        }
+        Action::Move(valve) => {
+            state.my_position = valve;
+        }
+        Action::None => {}
+    }
+    match elefant_action {
+        Action::Open(valve) => {
+            state.opened.insert(valve);
+        }
+        Action::Move(valve) => {
+            state.my_position = valve;
+        }
+        Action::None => {}
+    }
+    state.time_left -= 1;
+
+    if state.time_left == 0 {
+        return 0;
+    }
+
+    // Check cache if this state has already been
+    if let Some(max_pressure) = cache.get(&state) {
+        return *max_pressure;
+    }
+
+    let mut max_left_to_release = 0;
+
+    // Don't run around if all working valves are already opened
+    if state.opened.len() <= valves.iter().filter(|(_, v)| v.flow_rate > 0).count() {
+        let my_valve = valves.get(state.my_position).unwrap();
+        let elefant_valve = valves.get(state.elefant_position).unwrap();
+
+        // Investigate all possible moves
+        let mut my_actions: Vec<Action> = vec![];
+        if !state.opened.contains(my_valve.name) && my_valve.flow_rate > 0 {
+            my_actions.push(Action::Open(my_valve.name));
+        }
+        my_actions.extend(my_valve.tunnels.iter().map(|v| Action::Move(v)));
+
+        let mut elefant_actions: Vec<Action> = vec![];
+        if !state.opened.contains(elefant_valve.name) && my_valve.flow_rate > 0 {
+            elefant_actions.push(Action::Open(elefant_valve.name));
+        }
+        elefant_actions.extend(elefant_valve.tunnels.iter().map(|v| Action::Move(v)));
+
+        // Create all possible permutations of actions
+        let mut actions = vec![];
+        for my in &my_actions {
+            for elefant in &elefant_actions {
+                actions.push((my, elefant));
+            }
+        }
+
+        max_left_to_release = actions
+            .iter()
+            .map(|(my, elefant)| {
+                max_pressure_released_with_elefant(state.clone(), valves, cache, my, elefant)
+            })
+            .max()
+            .unwrap();
+    }
+
+    // Calculate pressure released this time step
+    let pressure_released_now = state
+        .opened
+        .iter()
+        .map(|v| valves.get(v).unwrap().flow_rate)
+        .sum::<u64>();
+
+    // Store result in cache if needed again
+    cache.insert(state, pressure_released_now + max_left_to_release);
+
+    pressure_released_now + max_left_to_release
 }
 
 // Recursive search algorithm with a HashMap for caching state
@@ -29,7 +117,7 @@ fn max_pressure_released<'a>(
             state.time_left -= 1;
         }
         Action::Move(valve) => {
-            state.position = valve;
+            state.my_position = valve;
             state.time_left -= 1;
         }
         Action::None => {}
@@ -48,7 +136,7 @@ fn max_pressure_released<'a>(
 
     // Don't run around if all working valves are already opened
     if state.opened.len() <= valves.iter().filter(|(_, v)| v.flow_rate > 0).count() {
-        let valve = valves.get(state.position).unwrap();
+        let valve = valves.get(state.my_position).unwrap();
 
         // Investigate all possible moves
         let max_move = valve
@@ -60,11 +148,13 @@ fn max_pressure_released<'a>(
 
         // Investigate open
         let mut max_open = 0;
-        if valve.flow_rate > 0 && !state.opened.contains(state.position) {
-            let mut opened = state.opened.clone();
-            opened.insert(state.position);
-            max_open =
-                max_pressure_released(state.clone(), valves, cache, &Action::Open(state.position));
+        if valve.flow_rate > 0 && !state.opened.contains(state.my_position) {
+            max_open = max_pressure_released(
+                state.clone(),
+                valves,
+                cache,
+                &Action::Open(state.my_position),
+            );
         }
 
         max_left_to_release = max_move.max(max_open);
@@ -84,16 +174,13 @@ fn max_pressure_released<'a>(
 }
 
 // Solve part1
-fn task1(valves: &Vec<Valve>) -> u64 {
-    let valves: HashMap<&str, Valve> = valves.iter().map(|v| (v.name, v.clone())).collect();
-
-    //dbg!(&valves);
-
+fn task1(valves: &HashMap<&str, Valve>) -> u64 {
     let mut cache: HashMap<State, u64> = HashMap::new();
 
     max_pressure_released(
         State {
-            position: "AA",
+            my_position: "AA",
+            elefant_position: "AA",
             time_left: 30,
             opened: BTreeSet::new(),
         },
@@ -103,12 +190,26 @@ fn task1(valves: &Vec<Valve>) -> u64 {
     )
 }
 
-fn task2(_input: &Vec<Valve>) -> u64 {
-    0
+fn task2(valves: &HashMap<&str, Valve>) -> u64 {
+    let mut cache: HashMap<State, u64> = HashMap::new();
+
+    max_pressure_released_with_elefant(
+        State {
+            my_position: "AA",
+            elefant_position: "AA",
+            time_left: 30 - 4,
+            opened: BTreeSet::new(),
+        },
+        &valves,
+        &mut cache,
+        &Action::None,
+        &Action::None,
+    )
 }
 
 pub fn solve_task(input: &str) -> (u64, u64) {
     let valves = parse_valves(input).unwrap().1;
+    let valves: HashMap<&str, Valve> = valves.iter().map(|v| (v.name, v.clone())).collect();
 
     let task1 = task1(&valves);
     let task2 = task2(&valves);
@@ -133,10 +234,10 @@ mod tests {
         Valve II has flow rate=0; tunnels lead to valves AA, JJ
         Valve JJ has flow rate=21; tunnel leads to valve II"#;
 
-        let (example1, _example2) = solve_task(input);
+        let (example1, example2) = solve_task(input);
 
         assert_eq!(example1, 1651);
-        //assert_eq!(example2, 0);
+        assert_eq!(example2, 1707);
     }
 
     #[test]
@@ -148,9 +249,9 @@ mod tests {
         )
         .unwrap();
 
-        let (task1, _task2) = solve_task(&input);
+        let (task1, task2) = solve_task(&input);
 
         assert_eq!(task1, 1673);
-        //assert_eq!(task2, 0);
+        assert_eq!(task2, 0);
     }
 }
