939adf1577399832e3e8a53c8ca20e3471ca93ac
LHelge <249865+LHelge@users.noreply.github.com>
2023-12-03 16:29:20 +0100

    Solved 2021 Day 7

---
+++ 2021/src/bin/y21d07.rs
@@ -0,0 +1,64 @@
+fn solve_task(input: &str) -> (u64, u64) {
+    let positions: Vec<u64> = input
+        .trim()
+        .split(',')
+        .map(|p| p.parse().unwrap())
+        .collect();
+
+    let min = *positions.iter().min().unwrap();
+    let max = *positions.iter().max().unwrap();
+
+    let mut task1 = u64::MAX;
+    for pos in min..=max {
+        let fuel: u64 = positions.iter().map(|p| p.abs_diff(pos)).sum();
+        if fuel < task1 {
+            task1 = fuel;
+        }
+    }
+
+    let mut task2 = u64::MAX;
+    for pos in min..=max {
+        let fuel: u64 = positions
+            .iter()
+            .map(|p| {
+                // Calculate arithmetical sum of all numbers between p and pos
+                let diff = p.abs_diff(pos);
+                diff * (1 + diff) / 2
+            })
+            .sum();
+        if fuel < task2 {
+            task2 = fuel;
+        }
+    }
+
+    (task1, task2)
+}
+
+fn main() {
+    let input = aoc::get_input(
+        2021,
+        7,
+        &std::env::var("SESSION").expect("SESSION environment variable not set"),
+    )
+    .unwrap();
+
+    let (task1, task2) = solve_task(&input);
+
+    println!("Task 1: {}", task1);
+    println!("Task 2: {}", task2);
+}
+
+#[cfg(test)]
+mod y2021d07 {
+    use super::*;
+
+    #[test]
+    fn examples() {
+        let example_input = "16,1,2,0,4,2,7,1,2,14";
+
+        let (example1, example2) = solve_task(example_input);
+
+        assert_eq!(example1, 37);
+        assert_eq!(example2, 168);
+    }
+}
