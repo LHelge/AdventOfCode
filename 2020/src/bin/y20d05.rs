1403d460126eb44659973cc13b58cd64d0bf120c
LHelge <249865+LHelge@users.noreply.github.com>
2024-11-12 23:33:33 +0100

    feat: Solved 2020 day 5

--- 2020/src/bin/y20d05.rs
+++ 2020/src/bin/y20d05.rs
@@ -1,8 +1,86 @@
 use aoc::AoCInput;
 
+enum FrontBack {
+    Front,
+    Back,
+}
 
-fn solve_task(_input: &str) -> (usize, usize) {
-    (0, 0)
+impl From<char> for FrontBack {
+    fn from(c: char) -> Self {
+        match c {
+            'F' => FrontBack::Front,
+            'B' => FrontBack::Back,
+            _ => panic!("Invalid FrontBack char"),
+        }
+    }
+}
+
+enum LeftRight {
+    Left,
+    Right,
+}
+
+impl From<char> for LeftRight {
+    fn from(c: char) -> Self {
+        match c {
+            'L' => LeftRight::Left,
+            'R' => LeftRight::Right,
+            _ => panic!("Invalid LeftRight char"),
+        }
+    }
+}
+
+fn calc_pos(input: &str) -> usize {
+    let rows = input
+        .chars()
+        .take(7)
+        .map(FrontBack::from)
+        .collect::<Vec<_>>();
+    let cols = input
+        .chars()
+        .skip(7)
+        .map(LeftRight::from)
+        .collect::<Vec<_>>();
+
+    let row = rows.iter().fold(0usize, |acc, fb| {
+        (acc << 1)
+            | match fb {
+                FrontBack::Front => 0,
+                FrontBack::Back => 1,
+            }
+    });
+    let col = cols.iter().fold(0usize, |acc, c| {
+        (acc << 1)
+            | match c {
+                LeftRight::Left => 0,
+                LeftRight::Right => 1,
+            }
+    });
+
+    row * 8 + col
+}
+
+fn solve_task(input: &str) -> (usize, usize) {
+    let mut seats = input.lines().map(calc_pos).collect::<Vec<_>>();
+    seats.sort_unstable();
+    seats.dedup();
+
+    let task1 = *seats.iter().last().unwrap();
+
+    let task2 = seats
+        .windows(3)
+        .find_map(|w| {
+            if w[0] + 1 != w[1] {
+                Some(w[0] + 1)
+            } else if w[1] + 1 != w[2] {
+                Some(w[1] + 1)
+            } else {
+                None
+            }
+        })
+        .unwrap();
+
+    (task1, task2)
 }
 
 fn main() {
