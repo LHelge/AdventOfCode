2a6f85cd93d36dfabcb8894473a13838857bb359
LHelge <249865+LHelge@users.noreply.github.com>
2024-12-01 12:49:34 +0100

    feat: solved 2024 day 1

--- 2024/src/bin/y24d01.rs
+++ 2024/src/bin/y24d01.rs
@@ -1,14 +1,48 @@
 use aoc::AoCInput;
 
-fn solve_task(input: &str) -> (u32, u32) {
-    let (task1, task2) = input.split_once(' ').unwrap();
-    (task1.parse().unwrap(), task2.parse().unwrap())
+fn parse_line(line: &str) -> (u64, u64) {
+    let (a, b) = line.split_once(' ').unwrap();
+    (a.trim().parse().unwrap(), b.trim().parse().unwrap())
+}
+
+fn parse(input: &str) -> (Vec<u64>, Vec<u64>) {
+    let vecs = input.lines().map(parse_line).unzip();
+
+    (vecs.0, vecs.1)
+}
+
+fn task1(mut list1: Vec<u64>, mut list2: Vec<u64>) -> u64 {
+    list1.sort();
+    list2.sort();
+
+    list1
+        .iter()
+        .zip(list2.iter())
+        .map(|(&l1, &l2)| l1.abs_diff(l2))
+        .sum()
+}
+
+fn task2(list1: &[u64], list2: &[u64]) -> u64 {
+    list1
+        .iter()
+        .map(|&l1| list2.iter().filter(|&&l2| l1 == l2).count() as u64 * l1)
+        .sum()
+}
+
+fn solve_task(input: &str) -> (u64, u64) {
+    let (list1, list2) = parse(input);
+
+    // Do task 2 first since there is no need to own the lists
+    let task2 = task2(&list1, &list2);
+    let task1 = task1(list1, list2);
+
+    (task1, task2)
 }
 
 fn main() {
     let input = AoCInput::from_env()
-    .get_input(2024, 1)
-    .expect("Could not fetch input");
+        .get_input(2024, 1)
+        .expect("Could not fetch input");
 
     let (task1, task2) = solve_task(&input);
 
@@ -22,12 +56,16 @@ mod y2024d01 {
 
     #[test]
     fn examples() {
-        // Task 1
-        let (example1, _) = solve_task("1 2");
-        assert_eq!(example1, 1);
+        let input = r#"3   4
+4   3
+2   5
+1   3
+3   9
+3   3"#;
 
-        // Task 2
-        let (_, example2) = solve_task("1 2");
-        assert_eq!(example2, 2);
+        // Task 1
+        let (example1, example2) = solve_task(input);
+        assert_eq!(example1, 11);
+        assert_eq!(example2, 31);
     }
 }
