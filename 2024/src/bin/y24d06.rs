ffefbbf09b492f5562a92d479c5d0217520fc7e8
LHelge <249865+LHelge@users.noreply.github.com>
2024-12-05 22:56:10 +0100

    chore: prepare for 2024 day 6

---
+++ 2024/src/bin/y24d06.rs
@@ -0,0 +1,44 @@
+const YEAR: u16 = 2024;
+const DAY: u8 = 6;
+use aoc::*;
+
+type ResultType = usize;
+type DataType = ();
+
+fn parse(_input: &str) -> Result<DataType> {
+    Ok(())
+}
+
+fn task1(_data: &DataType) -> Result<ResultType> {
+    Err(AoCError::Unsolved)
+}
+
+fn task2(_data: &DataType) -> Result<ResultType> {
+    Err(AoCError::Unsolved)
+}
+
+fn main() -> Result<()> {
+    let mut solution = Solution::<ResultType, DataType>::new(&parse, &task1, &task2);
+    solution.solve_for_answer(YEAR, DAY)?;
+
+    println!("Advent of Code {YEAR} day {DAY}");
+    println!("-------------------------");
+    println!("{solution}");
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn examples() {
+        let input = r#""#;
+
+        let mut solution = Solution::<ResultType, DataType>::new(&parse, &task1, &task2);
+        let (task1, task2) = solution.solve_for_test(input).unwrap();
+        assert_eq!(task1, None);
+        assert_eq!(task2, None);
+    }
+}
