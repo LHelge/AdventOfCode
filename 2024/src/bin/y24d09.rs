4579a67e2151a9c468e0552778e1e097d38bad30
LHelge <249865+LHelge@users.noreply.github.com>
2024-12-10 17:01:34 +0100

    feat: Solved 2024 day 9 part 2

--- 2024/src/bin/y24d09.rs
+++ 2024/src/bin/y24d09.rs
@@ -1,38 +1,74 @@
 const YEAR: u16 = 2024;
 const DAY: u8 = 9;
+use std::cmp::Ordering;
+
 use aoc::*;
 
-#[derive(Debug, Clone)]
-struct File {
-    id: usize,
-    start: usize,
-    size: usize,
-    space: usize,
+trait HashChecksum {
+    fn checksum(&self) -> usize;
 }
 
-impl File {
-    fn end(&self) -> usize {
-        self.start + self.size
-    }
-
+impl HashChecksum for Vec<DiskEntity> {
     fn checksum(&self) -> usize {
         let mut checksum = 0;
-        for i in self.start..(self.start + self.size) {
-            checksum += i * self.id;
+        let mut block = 0usize;
+
+        for &de in self.iter() {
+            match de {
+                DiskEntity::File { size, id } => {
+                    for _ in 0..size {
+                        checksum += block * id;
+                        block += 1;
+                    }
+                },
+                DiskEntity::Space { space } => {
+                    block += space;
+                }
+            }
         }
+
         checksum
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+enum DiskEntity {
+    Space{space: usize},
+    File{ size: usize, id: usize}
+}
+
+impl DiskEntity {
+    fn is_space(&self) -> bool {
+        matches!(self, DiskEntity::Space { space: _ })
+    }
+}
+
+fn print_disk(disk: &[DiskEntity]) {
+    for de in disk.iter() {
+        match *de {
+            DiskEntity::File { size, id } => {
+                for _ in 0..size {
+                    print!("{id}");
+                }
+            },
+            DiskEntity::Space { space } => {
+                for _ in 0..space {
+                    print!(".");
+                }
+            }
+        }
+    }
+    println!();
+}
+
 type ResultType = usize;
-type DataType = Vec<File>;
+type DataType = Vec<DiskEntity>;
 
 fn parse(input: &str) -> Result<DataType> {
-    let mut files = vec![];
+    let mut disk = vec![];
 
     let mut chars = input.chars();
     let mut id = 0;
-    let mut start = 0;
     while let (Some(size), space) = (chars.next(), chars.next()) {
         let size = size.to_digit(10).ok_or(AoCError::BadInput)? as usize;
         let space = space
@@ -40,67 +76,102 @@ fn parse(input: &str) -> Result<DataType> {
             .to_digit(10)
             .ok_or(AoCError::BadInput)? as usize;
 
-        files.push(File {
-            id,
-            start,
-            size,
-            space,
-        });
-
+        disk.push(DiskEntity::File { size, id });
         id += 1;
-        start += size + space;
+
+        if space > 0 {
+            disk.push(DiskEntity::Space { space });
+        }
     }
 
-    Ok(files)
+    Ok(disk)
 }
 
 fn task1(data: &DataType) -> Result<ResultType> {
-    let mut disk: Vec<Option<usize>> = vec![];
+    let mut disk = data.clone();
 
-    for file in data {
-        for _ in 0..file.size {
-            disk.push(Some(file.id));
-        }
-        for _ in 0..file.space {
-            disk.push(None);
-        }
-    }
+    while disk.iter().any(DiskEntity::is_space) {
+        if let Some(DiskEntity::File { size, id }) = disk.pop() {
+            let mut size_left = size;
 
-    while disk.iter().any(|f| f.is_none()) {
-        if let Some(last) = disk.pop() {
-            if let Some(first_empty_index) = disk.iter().position(|f| f.is_none()) {
-                disk.remove(first_empty_index);
-                disk.insert(first_empty_index, last);
+            while size_left > 0 {
+                if let Some(index) = disk.iter().position(DiskEntity::is_space) {
+                    if let DiskEntity::Space { space } = disk[index] {
+                        match space.cmp(&size_left) {
+                            Ordering::Less => {
+                                // Space is smaller than file, take chunk
+                                disk.remove(index);
+                                disk.insert(index, DiskEntity::File { size: space, id });
+                                size_left -= space;
+                            },
+                            Ordering::Greater => {
+                                // Space is larger than file, replace part of it
+                                disk.remove(index);
+                                disk.insert(index, DiskEntity::File { size: size_left, id });
+                                disk.insert(index+1, DiskEntity::Space { space: space - size_left });
+                                size_left = 0;
+                            }
+                            Ordering::Equal => {
+                                 //space and file are same size, replace it
+                            disk.remove(index);
+                            disk.insert(index, DiskEntity::File { size: size_left, id });
+                            size_left = 0;
+                            }
+                        }
+                    }
+                } else {
+                    // No free space left, put last.
+                    disk.push(DiskEntity::File { size: size_left, id});
+                    size_left = 0;
+                };
             }
         }
     }
 
-    Ok(disk.iter().enumerate().map(|(i, f)| i * f.unwrap()).sum())
+    Ok(disk.checksum())
 }
 
 fn task2(data: &DataType) -> Result<ResultType> {
     let mut disk = data.clone();
 
-    for id in (1..data.len()).rev() {
-        if let Some(first_free_id) = disk
-            .iter()
-            .position(|f| f.space >= disk[id].size && f.start < disk[id].start)
-        {
-            // Give free space to the file before
-            disk[id - 1].space += disk[id].size + disk[id].space;
-
-            // Move the file to the first free space
-            disk[id].start = disk[first_free_id].end();
-            disk[id].space = disk[first_free_id].space - disk[id].size;
-
-            // Remove the free space
-            disk[first_free_id].space = 0;
+    let mut ids = disk.iter().filter_map(|&de| {
+        match de {
+            DiskEntity::File { size: _, id } => Some(id),
+            _ => None,
         }
+    }).collect::<Vec<usize>>();
+    ids.sort();
 
-        disk.sort_by(|a, b| a.start.cmp(&b.start));
+    for &move_id in ids.iter().rev() {
+        if let Some(index) = disk.iter().position(|de| matches!(de, DiskEntity::File { size: _, id } if id == &move_id)) {
+            if let DiskEntity::File { size, id } = disk[index] {
+                if let Some(free_index) = disk.iter().position(|de| matches!(de, DiskEntity::Space { space } if space >= &size)) {
+                    if free_index < index {
+                        //println!("{index} <-> {free_index}");
+
+                        disk.remove(index);
+                        disk.insert(index, DiskEntity::Space { space: size });
+                        if let DiskEntity::Space { space } = disk.remove(free_index) {
+                            match space.cmp(&size) {
+                                Ordering::Greater => {
+                                    disk.insert(free_index, DiskEntity::File { size, id });
+                                    disk.insert(free_index+1, DiskEntity::Space { space: space - size });
+                                },
+                                Ordering::Equal => {
+                                    disk.insert(free_index, DiskEntity::File { size, id });
+                                },
+                                Ordering::Less => unreachable!(),
+                            }
+                        }
+
+                        //print_disk(&disk);
+                    }
+                } 
+            }
+        }
     }
 
-    Ok(disk.iter().map(File::checksum).sum())
+    Ok(disk.checksum())
 }
 
 fn main() -> Result<()> {
@@ -125,6 +196,7 @@ mod tests {
         let mut solution = Solution::<ResultType, DataType>::new(&parse, &task1, &task2);
         let (task1, task2) = solution.solve_for_test(input).unwrap();
         assert_eq!(task1, Some(1928));
+        //assert_eq!(task2, Some(2858));
         assert_eq!(task2, Some(2858));
     }
 }
