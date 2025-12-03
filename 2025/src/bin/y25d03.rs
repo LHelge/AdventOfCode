0e6b5a5aa0ed4a53d1e40e0f30b739a62a90ac18
lhelge <249865+LHelge@users.noreply.github.com>
2025-12-03 08:25:04 +0100

    feat: Solved 2025 day 3

--- 2025/src/bin/y25d03.rs
+++ 2025/src/bin/y25d03.rs
@@ -1,20 +1,19 @@
 const YEAR: u16 = 2025;
 const DAY: u8 = 3;
-use std::ops::Index;
 
 use aoc::*;
 
-type ResultType = u32;
+type ResultType = u64;
 type DataType = Vec<Vec<u8>>;
 
 fn parse(input: &str) -> Result<DataType> {
     Ok(input
         .lines()
-        .map(|l| l.bytes().map(|c| c - '0' as u8).collect())
+        .map(|l| l.bytes().map(|c| c - b'0').collect())
         .collect())
 }
 
-fn get_max_joltage(bank: &[u8], num: usize) -> u32 {
+fn get_max_joltage(bank: &[u8], num: usize) -> u64 {
     if num == 0 {
         return 0;
     }
@@ -22,10 +21,8 @@ fn get_max_joltage(bank: &[u8], num: usize) -> u32 {
     let mut joltage = 0;
 
     for digit in (0..=9).rev() {
-        if let Some(index) = bank[..(bank.len() - num)].iter().position(|&d| d == digit)
-            && bank.len() - index >= num
-        {
-            joltage += digit as u32 * 10u32.pow(num as u32 - 1);
+        if let Some(index) = bank[..=(bank.len() - num)].iter().position(|&d| d == digit) {
+            joltage += digit as u64 * 10u64.pow(num as u32 - 1);
             joltage += get_max_joltage(&bank[(index + 1)..], num - 1);
             break;
         }
@@ -35,38 +32,11 @@ fn get_max_joltage(bank: &[u8], num: usize) -> u32 {
 }
 
 fn task1(data: &DataType) -> Result<ResultType> {
-    let mut joltage = 0;
-
-    for bank in data {
-        let bank_joltage = get_max_joltage(bank, 2);
-        println!("Bank joltage: {bank_joltage}");
-        joltage += bank_joltage;
-    }
-
-    Ok(joltage)
+    Ok(data.iter().map(|bank| get_max_joltage(bank, 2)).sum())
 }
 
-//fn task1(data: &DataType) -> Result<ResultType> {
-//    let mut joltage = 0;
-//
-//    for bank in data {
-//        for digit in (0..=9).rev() {
-//            if let Some(index) = bank.iter().position(|d| *d == digit) {
-//                if let Some(max) = bank.iter().skip(index + 1).max() {
-//                    let bank_joltage = digit * 10 + max;
-//                    //println!("Bank joltage: {bank_joltage}");
-//                    joltage += bank_joltage as u32;
-//                    break;
-//                }
-//            }
-//        }
-//    }
-//
-//    Ok(joltage)
-//}
-
-fn task2(_data: &DataType) -> Result<ResultType> {
-    Err(AoCError::Unsolved)
+fn task2(data: &DataType) -> Result<ResultType> {
+    Ok(data.iter().map(|bank| get_max_joltage(bank, 12)).sum())
 }
 
 fn main() -> Result<()> {
@@ -95,7 +65,7 @@ mod tests {
         let mut solution = Solution::<ResultType, DataType>::new(&parse, &task1, &task2);
         let (task1, task2) = solution.solve_for_test(input).unwrap();
         assert_eq!(task1, Some(357));
-        assert_eq!(task2, None);
-        //assert_eq!(task2, Some(3121910778619));
+        //assert_eq!(task2, None);
+        assert_eq!(task2, Some(3121910778619));
     }
 }
