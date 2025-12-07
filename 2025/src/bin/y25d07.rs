27f1b223d4d0fa24a20b71ee03e03122dd74e3e9
LHelge <249865+LHelge@users.noreply.github.com>
2025-12-07 08:43:34 +0100

    chore: Cleaned up further

--- 2025/src/bin/y25d07.rs
+++ 2025/src/bin/y25d07.rs
@@ -40,10 +40,10 @@ impl AoCProblem<usize, usize> for Problem {
     fn part1(&self) -> Result<usize> {
         let (start, _) = self.diagram.find(&Entry::Start).ok_or(AoCError::BadInput)?;
 
-        let mut beams: HashSet<usize> = HashSet::new();
-        beams.insert(start.x);
+        let mut beams: HashSet<Position> = HashSet::new();
+        beams.insert(start + Direction::South);
 
-        count_splits(&self.diagram, start.y + 1, beams)
+        count_splits(&self.diagram, beams)
     }
 
     fn part2(&self) -> Result<usize> {
@@ -56,28 +56,25 @@ impl AoCProblem<usize, usize> for Problem {
     }
 }
 
-fn count_splits(diagram: &Vec2d<Entry>, line: usize, beams: HashSet<usize>) -> Result<usize> {
-    if line >= diagram.height() {
-        return Ok(0);
-    }
-
+fn count_splits(diagram: &Vec2d<Entry>, beams: HashSet<Position>) -> Result<usize> {
     let mut new_beams = HashSet::new();
     let mut splits = 0;
 
     for beam in beams {
-        let pos = Position::new(beam, line);
-        match diagram.get(pos) {
-            Some(Entry::EmptySpace) => new_beams.insert(pos.x),
+        let below = beam + Direction::South;
+        match diagram.get(below) {
+            Some(Entry::EmptySpace) => new_beams.insert(below),
             Some(Entry::Manifold) => {
                 splits += 1;
-                new_beams.insert(pos.x - 1);
-                new_beams.insert(pos.x + 1)
+                new_beams.insert(below + Direction::East);
+                new_beams.insert(below + Direction::West)
             }
+            None => return Ok(0),
             _ => Err(AoCError::BadInput)?,
         };
     }
 
-    Ok(count_splits(diagram, line + 1, new_beams)? + splits)
+    Ok(count_splits(diagram, new_beams)? + splits)
 }
 
 fn count_timelines(
